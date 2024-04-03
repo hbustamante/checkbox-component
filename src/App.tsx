cd793693e07370e5c5492523836d379140a19427
heber.bustamante <tech-heber.bustamante@spsa.pe>
2024-04-03 11:29:07 -0300

    Agrega funcionalidad de selección de países

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+interface Country {
+  name: string;
+  checked: boolean;
+}
+
+const App: React.FC = () => {
+  const [countries, setCountries] = useState<Country[]>([
+    { name: 'India', checked: false },
+    { name: 'Usa', checked: false },
+    { name: 'France', checked: false },
+    // Agrega más países aquí
+  ]);
+
+  const [selectAll, setSelectAll] = useState<boolean>(false);
+
+  const handleCheck = (index: number): void => {
+    const newCountries = [...countries];
+    newCountries[index].checked = !newCountries[index].checked;
+    setCountries(newCountries);
+
+    const allSelected = newCountries.every(country => country.checked);
+    setSelectAll(allSelected);
+  };
+
+  const handleSelectAllCheck = (): void => {
+    setSelectAll(!selectAll);
+    const newCountries = countries.map(country => ({ ...country, checked: !selectAll }));
+    setCountries(newCountries);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <label>
+          <input
+            type="checkbox"
+            checked={selectAll}
+            onChange={handleSelectAllCheck}
+          />
+          Seleccionar todos
+        </label>
+      </div>
+      {countries.map((country, index) => (
+        <div key={index}>
+          <label>
+            <input
+              type="checkbox"
+              checked={country.checked}
+              onChange={() => handleCheck(index)}
+            />
+            {country.name}
+          </label>
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
